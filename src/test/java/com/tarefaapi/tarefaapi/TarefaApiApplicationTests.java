ab3b6c9a87b7bce7b36c27b493ac5e57e5d15029
David Braz <davidbrazdasilva@gmail.com>
2025-01-30 09:18:04 -0300

    first commit

---
+++ src/test/java/com/tarefaapi/tarefaapi/TarefaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarefaapi.tarefaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarefaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
